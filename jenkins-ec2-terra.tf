a58bcc36d2e4afd1c4265e5ccf9b9d258f3490b4
dhairyashild <125667366+dhairyashild@users.noreply.github.com>
2025-03-12 12:23:54 +0530

    Update jenkins-ec2-terra.tf

--- jenkins-ec2-terra.tf
+++ jenkins-ec2-terra.tf
@@ -11,8 +11,9 @@ resource "aws_instance" "jenkin-ec3" {
 sudo apt update -y
 sudo apt install fontconfig openjdk-17-jre -y
 java -version
-
-sudo apt-get update -y
+openjdk version "17.0.13" 2024-10-15
+OpenJDK Runtime Environment (build 17.0.13+11-Debian-2)
+OpenJDK 64-Bit Server VM (build 17.0.13+11-Debian-2, mixed mode, sharing)
 
 sudo wget -O /usr/share/keyrings/jenkins-keyring.asc \
   https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key
@@ -22,7 +23,6 @@ echo "deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc]" \
   /etc/apt/sources.list.d/jenkins.list > /dev/null
 
 sudo apt-get update -y
-
 sudo apt-get install jenkins -y
 
 
